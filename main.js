86a0b93fb5f53e186334320ee17566f104fefcf8
Fernanda Ramírez <mframirezfuentes@gmail.com>
2022-12-20 12:03:38 -0300

    Creación de un formulario y el evento addEventListener

--- main.js
+++ main.js
@@ -3,9 +3,13 @@ const input1 = document.querySelector("#calculo1")
 const input2 = document.querySelector("#calculo2")
 const btn = document.querySelector("#btnCalcular")
 let resultado = document.getElementById('resultado')
+let form = document.querySelector('#formulario')
 
 
-function btnOnClick(){
+form.addEventListener('submit', sumarValores)
+
+function sumarValores(event){
+    event.preventDefault()
    let result = parseInt(input1.value) + parseInt(input2.value)
-    resultado.append(result)
+    resultado.append('El resultado es: '+result)
 }
