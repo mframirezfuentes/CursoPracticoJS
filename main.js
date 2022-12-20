fb67525aa3676ba5beff3aea0ae83ef045f108dd
Fernanda Ramírez <mframirezfuentes@gmail.com>
2022-12-20 10:57:31 -0300

    Agregandole dinamismo a la página con los query selectos

--- main.js
+++ main.js
@@ -1,24 +1,11 @@
 const h1 = document.querySelector("h1")
-const p = document.querySelector("p")
-const parrafito = document.querySelector(".parrafito")
-const pid = document.querySelector("#pid")
-const input = document.querySelector("input")
+const input1 = document.querySelector("#calculo1")
+const input2 = document.querySelector("#calculo2")
+const btn = document.querySelector("#btnCalcular")
+let resultado = document.getElementById('resultado')
 
-console.log(input.value);
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input
-});
 
-h1.classList.add("rojo")
-h1.classList.add("verde")
-
-input.value ="456"
-
-const img = document.createElement("img")
-img.setAttribute("src", 'https://i.pinimg.com/originals/8b/02/40/8b02409f975c98c89e15bc089e3bd289.gif')
-console.log(img);
-pid.append(img)
+function btnOnClick(){
+   let result = parseInt(input1.value) + parseInt(input2.value)
+    resultado.append(result)
+}
